12868136f0ed44637e2d78846c328e9042fb4cbe
Adriano <adriano.me283@gmail.com>
2024-06-14 19:24:58 -0300

    meu primeiro projeto java

--- Meu primeiro projeto java/src/exemploWhile.java
+++ Meu primeiro projeto java/src/exemploWhile.java
@@ -1,23 +1,20 @@
 import java.util.concurrent.ThreadLocalRandom;
 public class exemploWhile {
-     public static void main(String[] args) {
-        
-    double mesada = 50.0;
 
-        while(mesada > 0){
+    public static void main(String[] args) {
+        double mesada = 100.0;
+         while (mesada > 0) {
             double valorDoce = valorAleatorio();
+            if(valorDoce > mesada);
+            valorDoce = mesada;
 
-            if(valorDoce > mesada)
-                valorDoce = mesada;
-
-                System.out.println("Doce do valor: " + valorDoce + "adcionar no carrinho");
-                mesada = mesada - valorDoce;
-            }
-            System.out.println("Mesada " + mesada);
-            System.out.println("João gastou toda a mesa em doces");
-
-        }
-        private static double valorAleatorio() {
-            return ThreadLocalRandom.current().nextDouble(2, 13);
-        }
-     }
+            System.out.println("doce do valor :" + valorDoce + "adcionar no carrinho");
+            mesada = mesada - valorDoce;
+         }
+        System.out.println("Mesada" + mesada); 
+        System.out.println("Adriano gastou toda a mesada");
+    }
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(1, 12);
+    }
+}
